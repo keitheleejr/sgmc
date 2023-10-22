972b26e28bac8c87ce7a0adc8ee9cc7008e2e5de
Keith Lee <keilee@valdosta.edu>
2023-10-22 09:32:04 -0400

    Added health status plots

--- plan.R
+++ plan.R
@@ -2,10 +2,10 @@ library("plan")
 g <- new("gantt")
 g <- ganttAddTask(g, "Build Project Folder", "2023-10-15", "2023-10-16", done=100)
 g <- ganttAddTask(g, "Analysis") # no times, so a heading
-g <- ganttAddTask(g, "Clean Data", "2023-10-16", "2023-10-17", done=10)
-g <- ganttAddTask(g, "Add Levels to levels.R", "2023-10-16", "2023-10-17", done=50)
-g <- ganttAddTask(g, "Functions", "2023-10-16", "2023-10-17", done=75)
-g <- ganttAddTask(g, "EDA with ggplot2", "2023-10-18", "2023-10-22", done =0)
+g <- ganttAddTask(g, "Clean Data", "2023-10-16", "2023-10-17", done=100)
+g <- ganttAddTask(g, "Add Levels to levels.R", "2023-10-16", "2023-10-17", done=100)
+g <- ganttAddTask(g, "Functions", "2023-10-16", "2023-10-17", done=100)
+g <- ganttAddTask(g, "EDA with ggplot2", "2023-10-18", "2023-10-22", done = 50)
 g <- ganttAddTask(g, "Report") # no times, so a heading
 g <- ganttAddTask(g, "Shiny UI", "2023-10-16", "2023-10-19", done=60)
 g <- ganttAddTask(g, "Shiny Server", "2023-10-19", "2023-10-22", done=75)
@@ -16,7 +16,7 @@ font <- ifelse(is.na(g[["start"]]), 2, 1)
 
 
 plot(g, ylabel=list(font=font),
-     event.time="2023-10-16", event.label="Report Date")
+     event.time="2023-10-21", event.label="Report Date")
 par(lend="square") # default is round
 # legend("topright", pch=22, pt.cex=2, pt.bg=gray(c(0.3, 0.9)),
 #        border="black", xpd=NA,
